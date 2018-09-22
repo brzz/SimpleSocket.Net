e34513b882aaf5e7db6714a2cc919ef98383349d
LCQ <LCQ@LCQ-E460>
2018-09-22 10:05:35 +0800

    添加获取可用端口功能

--- SimpleSocket.Net/Program.cs
+++ SimpleSocket.Net/Program.cs
@@ -11,12 +11,45 @@ namespace SimpleSocket.Net
 {
     class Program
     {
+        
+        //public static void SetKeepAlive(ulong keepalive_time, ulong keepalive_interval)
+        //{
+        //    //mill-seconds
+        //    int bytes_per_long = 32 / 8;
+        //    byte[] keep_alive = new byte[3 * bytes_per_long];
+        //    ulong[] input_params = new ulong[3];
+        //    int i1;
+        //    int bits_per_byte = 8;
+
+        //    if (keepalive_time == 0 || keepalive_interval == 0)
+        //        input_params[0] = 0;
+        //    else
+        //        input_params[0] = 1;
+        //    input_params[1] = keepalive_time;
+        //    input_params[2] = keepalive_interval;
+        //    for (i1 = 0; i1 < input_params.Length; i1++)
+        //    {
+        //        keep_alive[i1 * bytes_per_long + 3] = (byte)(input_params[i1] >> ((bytes_per_long - 1) * bits_per_byte) & 0xff);
+        //        keep_alive[i1 * bytes_per_long + 2] = (byte)(input_params[i1] >> ((bytes_per_long - 2) * bits_per_byte) & 0xff);
+        //        keep_alive[i1 * bytes_per_long + 1] = (byte)(input_params[i1] >> ((bytes_per_long - 3) * bits_per_byte) & 0xff);
+        //        keep_alive[i1 * bytes_per_long + 0] = (byte)(input_params[i1] >> ((bytes_per_long - 4) * bits_per_byte) & 0xff);
+        //    }
+        //    //LogTrace.Trace(this, "Keep Alive Bits: {0}", ByteArrayFormater.HexDump(keep_alive));
+        //    //NetSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, keep_alive);
+        //}
+
         static void RecF(Socket Remote, byte[] Data, int RecInt)
         {
             Console.WriteLine(Encoding.UTF8.GetString(Data, 0, RecInt));
         }
 
-        static void reTry(Exception e, SocketHelp SocketThis)
+        static void Accpet(Socket Remote)
+        {
+            //byte[] KA = { 1,0,0,0,16,39,0,0,152,58,0,0};
+            //Remote.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, KA);
+            Remote.SetSocketKeepAliveValues(10000, 1000);
+        }
+        static void reTry(Exception e, SocketHelp SocketThis, Socket Remote)
         {
             try
             {
@@ -28,11 +61,14 @@ namespace SimpleSocket.Net
 
         static void Main(string[] args)
         {
-#region Server Demo
-            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            //byte[] KA = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //SetKeepAlive(10000, 15000);
+            #region Server Demo
+            IPAddress addr = IPAddress.Parse("0.0.0.0");
             IPEndPoint endp = new IPEndPoint(addr, 60231);
             SocketHelp S = new SocketHelp(endp, SocketHType.Server);
 
+            S.onAccept += Accpet;
             S.onPacketRec += RecF;
             S.onRecErr = reTry;
 
@@ -40,9 +76,10 @@ namespace SimpleSocket.Net
 
             S.Open();
             Console.ReadKey();
-#endregion
+            #endregion
 
-#region Client Demo
+#if  StartClient
+            #region Client Demo
             IPAddress addr2 = IPAddress.Parse("127.0.0.1");
             IPEndPoint endp2 = new IPEndPoint(addr, 60231);
             SocketHelp C = new SocketHelp(endp, SocketHType.Client);
@@ -53,7 +90,8 @@ namespace SimpleSocket.Net
 
             S.Open();
             Console.ReadKey();
-#endregion
+            #endregion
+#endif
         }
     }
 }
--- SimpleSocket.Net/SocketHelp.cs
+++ SimpleSocket.Net/SocketHelp.cs
@@ -1,25 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
 #pragma warning disable 0649
 
-
+/// <summary>
+/// 20180906 add keepalive on Accpet use
+/// </summary>
 namespace CsharpTest
 {
+    public static class SocketExtensions
+    {
+        public static void SetSocketKeepAliveValues(this Socket instance, int KeepAliveTime, int KeepAliveInterval)
+        {
+            //KeepAliveTime is ms
+            //KeepAliveTime: default value is 2hr
+            //KeepAliveInterval: default value is 1s and Detect 5 times
+
+            //the native structure
+            //struct tcp_keepalive {
+            //ULONG onoff;
+            //ULONG keepalivetime;
+            //ULONG keepaliveinterval;
+            //};
+
+            int size = System.Runtime.InteropServices.Marshal.SizeOf(new uint());
+            byte[] inOptionValues = new byte[size * 3]; // 4 * 3 = 12
+            bool OnOff = true;
+
+            BitConverter.GetBytes((uint)(OnOff ? 1 : 0)).CopyTo(inOptionValues, 0);
+            BitConverter.GetBytes((uint)KeepAliveTime).CopyTo(inOptionValues, size);
+            BitConverter.GetBytes((uint)KeepAliveInterval).CopyTo(inOptionValues, size * 2);
+
+            instance.IOControl(IOControlCode.KeepAliveValues, inOptionValues, null);
+        }
+    }
     public enum SocketHType
     {
         Server,
         Client
     }
 
-    class SocketHelp
+    public class SocketHelp
     {
-#region Common Var
+
+        #region Common Var
         private IPEndPoint endp;    
         private SocketHType type;
         private bool _Open;
@@ -31,10 +62,10 @@ namespace CsharpTest
         public delegate void _onPacketRec(Socket S, byte[] D, int revByte);
         public _onPacketRec onPacketRec;
 
-        public delegate void _onRecErr(Exception e, SocketHelp SocketThis);
+        public delegate void _onRecErr(Exception e, SocketHelp SocketThis, Socket Remote);
         public _onRecErr onRecErr;
 
-        public delegate void _onSendErr(Exception e, SocketHelp SocketThis);
+        public delegate void _onSendErr(Exception e, SocketHelp SocketThis, Socket Remote);
         public _onSendErr onSendErr;
 
         public SocketHelp(IPEndPoint endp, SocketHType type)
@@ -44,7 +75,8 @@ namespace CsharpTest
         }
 #endregion
 
-#region Common Function
+
+        #region Common Function
         public void Open()
         {
             if(SocketThis != null)
@@ -61,7 +93,7 @@ namespace CsharpTest
                 case SocketHType.Client: ClientConnect(); break;
             }
         }
-
+        
         public void ReOpen()
         {
             if(type == SocketHType.Client)
@@ -80,7 +112,7 @@ namespace CsharpTest
                 catch (Exception e)
                 {
                     if (onRecErr != null)
-                        onRecErr.Invoke(e, this);
+                        onRecErr.Invoke(e, this, this.SocketThis);
                 }
             }
             else
@@ -130,7 +162,7 @@ namespace CsharpTest
                     catch (Exception e)
                     {
                         if (this.onSendErr != null)
-                            this.onSendErr.Invoke(e, this);
+                            this.onSendErr.Invoke(e, this, this.SocketThis);
                     }
 
                 }
@@ -151,7 +183,7 @@ namespace CsharpTest
                         catch (Exception e)
                         {
                             if (this.onSendErr != null)
-                                this.onSendErr.Invoke(e, this);
+                                this.onSendErr.Invoke(e, this, this.ClientList[i].clientSocket);
                         }
                     }
                 }
@@ -159,9 +191,11 @@ namespace CsharpTest
             }
         }
 
-#endregion
+        #endregion
 
-#region ServerProcess
+
+        #region ServerProcess
+
         private void ServerAccept()
         {
             if (CntrolThread == null)
@@ -187,46 +221,62 @@ namespace CsharpTest
         }
 
         public List<ClientStatus> ClientList;
+
+        public delegate void _onAccept(Socket Remote);
+        public _onAccept onAccept;
         private static void _ServerAccept(Object arg)
         {
             SocketHelp _this = (SocketHelp)arg;
 
             _this.ClientList = new List<ClientStatus>();
 
-            _this.SocketThis = 
-                new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             _this.SocketThis.Bind(_this.endp);
             _this.SocketThis.Listen(20);
+
+            Console.WriteLine("Port:{0} Bind Success Wait Client...", _this.endp.Port);
 
             while (_this._Open)
             {
                 ClientStatus c = 
                     new ClientStatus(_this.SocketThis.Accept(), new Thread(ClientRec));
+
                 c._this = _this;
                 _this.ClientList.Add(c);
+
                 c.clientThread.Start(c);
             }
+
+
+            _this.ClientList.Clear();
+            _this.ClientList = null;
+
+            _this.SocketThis.Close();
         }
 
         private static void ClientRec(object _ClientStatus)
         {
             ClientStatus CS = (ClientStatus)_ClientStatus;
 
-            byte[] result = new byte[1572];
+            if(CS._this.onAccept != null)
+                CS._this.onAccept.Invoke(CS.clientSocket);//First Accept
+
+            byte[] result = new byte[1024*8];
 
             while (true)
             {
                 try
                 {
                     int receiveNumber = CS.clientSocket.Receive(result);
-                    if(CS._this.onPacketRec != null)
+                    if (receiveNumber == 0) throw new Exception("收到0字节数据");
+                    if(CS._this.onPacketRec != null && receiveNumber > 0)
                         CS._this.onPacketRec.Invoke
                             (CS.clientSocket, result, receiveNumber);
                 }
                 catch (Exception e)
                 {
+                    Console.WriteLine(e.Message);
                     if (CS._this.onRecErr != null)
-                        CS._this.onRecErr.Invoke(e, CS._this);
+                        CS._this.onRecErr.Invoke(e, CS._this, CS.clientSocket);
 
                     //remove unuse client
                     CS.clientSocket.Close();
@@ -238,7 +288,8 @@ namespace CsharpTest
         }
 #endregion
 
-#region ClientProcess
+
+        #region ClientProcess
         private void ClientConnect()
         {
             if (CntrolThread == null)
@@ -261,20 +312,20 @@ namespace CsharpTest
             catch (Exception e)
             {
                 if (_this.onRecErr != null)
-                    _this.onRecErr.Invoke(e, _this);
+                    _this.onRecErr.Invoke(e, _this, _this.SocketThis);
             }
 
 
 
-            byte[] buffer = new byte[1572];
+            byte[] buffer = new byte[1024*8];
             int recByte = 0;
-            while (_this._Open)
+            while (_this._Open && _this.onPacketRec!= null)
             {
                 try
                 {
                     recByte = _this.SocketThis.Receive(buffer);
-                    if (recByte < 1) throw new SocketException();
-                    if (_this.onPacketRec != null)
+                    if (recByte <= 0) throw new SocketException();
+                    if (_this.onPacketRec != null && recByte > 0)
                         _this.onPacketRec.Invoke(_this.SocketThis, buffer, recByte);
                     recByte = 0;
                     //threadclient.Start();
@@ -282,12 +333,84 @@ namespace CsharpTest
                 catch (Exception e)
                 {
                     if (_this.onRecErr != null)
-                        _this.onRecErr.Invoke(e, _this);
+                        _this.onRecErr.Invoke(e, _this, _this.SocketThis);
+                    else _this._Open = false;
                 }
             }
         }
 
-#endregion
+        #endregion
 
+
+        #region Get Free Port
+        /// <summary>
+        /// 获取第一个可用的端口号
+        /// </summary>
+        /// <returns></returns>
+        public static int GetFirstAvailablePort()
+        {
+            int MAX_PORT = 59999; //系统tcp/udp端口数最大是65535            
+            int BEGIN_PORT = 30000;//从这个端口开始检测
+
+            Random rd = new Random();
+            int n = rd.Next(BEGIN_PORT, MAX_PORT-((MAX_PORT - BEGIN_PORT)/2));
+            for (int i = n; i < MAX_PORT; i++)
+            {
+                if (PortIsAvailable(i)) return i;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// 获取操作系统已用的端口号
+        /// </summary>
+        /// <returns></returns>
+        public static IList PortIsUsed()
+        {
+            //获取本地计算机的网络连接和通信统计数据的信息
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //返回本地计算机上的所有Tcp监听程序
+            IPEndPoint[] ipsTCP = ipGlobalProperties.GetActiveTcpListeners();
+
+            //返回本地计算机上的所有UDP监听程序
+            IPEndPoint[] ipsUDP = ipGlobalProperties.GetActiveUdpListeners();
+
+            //返回本地计算机上的Internet协议版本4(IPV4 传输控制协议(TCP)连接的信息。
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            IList allPorts = new ArrayList();
+            foreach (IPEndPoint ep in ipsTCP) allPorts.Add(ep.Port);
+            foreach (IPEndPoint ep in ipsUDP) allPorts.Add(ep.Port);
+            foreach (TcpConnectionInformation conn in tcpConnInfoArray) allPorts.Add(conn.LocalEndPoint.Port);
+
+            return allPorts;
+        }
+
+        /// <summary>
+        /// 检查指定端口是否已用
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static bool PortIsAvailable(int port)
+        {
+            bool isAvailable = true;
+
+            IList portUsed = PortIsUsed();
+
+            foreach (int p in portUsed)
+            {
+                if (p == port)
+                {
+                    isAvailable = false; break;
+                }
+            }
+
+            return isAvailable;
+        }
+        #endregion
     }
 }
+
+#pragma warning restore
